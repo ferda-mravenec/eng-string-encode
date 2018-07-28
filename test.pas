6d09c519b12134ab0de2e8762ec76f60441c2f02
ferda-mravenec <41809007+ferda-mravenec@users.noreply.github.com>
2018-07-28 01:26:35 +0000

    Main file to run project
    This is just an example how to use the file transform.pas. One bug with the SaveToFile, writes binary instead text data.

---
+++ test.pas
@@ -0,0 +1,79 @@
+unit Test;
+
+interface
+
+uses Classes, SysUtils, Transform;
+
+var sArr, allBytes, content: TBytes;
+sss: String;
+len : byte;
+sumLen: word;
+buffer: string;
+o1, o2: longword; // offsety
+
+begin
+
+  AddList := TStringList.create;
+  SourceList := TStringList.create;
+  SourceList.LoadFromFile('a:\source_eng.txt');
+
+  sumLen := 0;
+  for i:=0 to SourceList.count-1 do
+    begin
+      sss := SourceList[i];
+      len := encode(sss, sArr);
+      setLength(allBytes,sumLen+len );
+      move(sArr[0], allBytes[sumLen], len);
+      sumLen := sumLen + len;
+    end;
+  // WRITE
+  with
+    TFileStream.Create('A:\test.bin', fmCreate)
+    do
+      try
+        seek(0, soBeginning);
+        Write(allBytes[0], sumLen ) // zapsat výstupní pole
+      finally
+        free;
+      end;
+
+  with
+    TFileStream.Create('A:\test.bin', fmOpenRead)
+    do
+      try
+        setLength(content, size);
+        seek(0, soBeginning);
+        readbuffer(content[0], size ); // zapsat výstupní pole
+        o1 := 0;
+        for i := 0 to size-1 do
+          begin
+          // o2 := o2 + 1;
+          o2 := o2;
+          if content[i]=0 then
+            begin
+              sss := '';
+              o2 := i;
+              if (o2-o1) > 0 then
+                begin
+                  setLength(sArr, o2-o1);
+                  move(content[o1],sArr[0], o2-o1 );
+                  decode(sArr, sss);
+                  AddList.add(sss);
+                end;
+              {
+              else
+                begin
+                  sss := sss + chr( content[o1] );
+                end;
+              }
+              o1 := i + 1 ; // next begin is after zero
+            end;
+          end;
+
+      finally
+        free;
+      end;
+
+  AddList.saveToFile('B:\output.txt');
+
+end.
